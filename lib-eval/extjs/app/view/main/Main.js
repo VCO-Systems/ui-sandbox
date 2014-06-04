e01512fef01bd3bda026c1f62c2116b4843780a1
Van <van.cahall@vcosystems.com>
2014-06-04 01:12:12 -0400

    added most of the controls on the List screen for extjs

--- lib-eval/extjs/app/view/main/Main.js
+++ lib-eval/extjs/app/view/main/Main.js
@@ -96,10 +96,15 @@ Ext.define('extjs.view.main.Main', {
         },
         {
             title: 'The Data',
-            layout: 'fit',
+            //layout: 'fit',
             items: [{
                 xtype: 'grid',
                 title: 'Simpsons',
+                selType: 'checkboxmodel',
+                selModel: {
+                	checkOnly: true,
+                	injectCheckbox: 0
+                },
                 store: {
                     fields:['name', 'email', 'phone'],
                     data:[
@@ -120,9 +125,71 @@ Ext.define('extjs.view.main.Main', {
                     { text: 'Name',  dataIndex: 'name' },
                     { text: 'Email', dataIndex: 'email', flex: 1},
                     { text: 'Phone', dataIndex: 'phone' }
-                ]
-            }]
+                ],
+             // paging bar on the bottom
+                bbar: Ext.create('Ext.PagingToolbar', {
+                    //store: store,
+                    displayInfo: true,
+                    displayMsg: 'Displaying topics {0} - {1} of {2}',
+                    emptyMsg: "No topics to display",
+                    items:[
+                        '-', {
+                        text: 'Show Preview',
+                        //pressed: pluginExpanded,
+                        enableToggle: true,
+                        toggleHandler: function(btn, pressed) {
+                            var preview = Ext.getCmp('gv').getPlugin('preview');
+                            preview.toggleExpanded(pressed);
+                        }
+                    }]
+                }),
+            }
+            
+            ]
         }]
     }]
 });
 
+Ext.define('User', {
+    extend: 'Ext.data.Model',
+    
+});
+
+var userStore = Ext.create('Ext.data.Store', {
+    model: 'User',
+    autoLoad: true,
+    pageSize: 4,
+    url: 'data/users/json',
+    reader: {
+    	type: 'json',
+    	root: 'users',
+    	totalProperty: 'total'
+    }
+});
+
+Ext.create('Ext.grid.Panel', {
+    store: userStore,
+    width: 400,
+    height: 200,
+    title: 'Application Users',
+    columns: [
+        {
+            text: 'Name',
+            width: 100,
+            sortable: false,
+            hideable: false,
+            dataIndex: 'name'
+        },
+        {
+            text: 'Email Address',
+            width: 150,
+            dataIndex: 'email',
+            hidden: true
+        },
+        {
+            text: 'Phone Number',
+            flex: 1,
+            dataIndex: 'phone'
+        }
+    ]
+});
